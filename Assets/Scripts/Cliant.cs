d6eb5d18736b6aea2df8aa1b2499d0045f8a3476
RLH107 <lucas.hellner08@gmail.com>
2025-03-09 12:39:44 -0300

    Manegador De Clientes
    O manejador de clientes é o script que maneja os pedidos dos clientes e as suas falas. Ele necessita de uma referencia a um objeto com TMPpro (Caixa de Texto) e ele precisa estar localizado no objeto com um colider Trigger que ira receber os pedidos.

---
+++ Assets/Scripts/Cliant.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cliant
+{
+    //script para guardar as informações de cada cliente
+    public string Demand, Right, Wrong;
+    private string Coffie;
+    
+    //Para criar este script é necessario as seguintes informações
+    public Cliant(string CliantDemand, string Coffie, string Right, string Wrong)
+    {
+        Demand = CliantDemand; //Pergunta do Cliente
+        this.Coffie = Coffie; //Identificação do produto requisitado pelo Cliente
+        this.Right = Right; //Resposta do produto certo
+        this.Wrong = Wrong; //resposta do produto errado
+    }
+    public override string ToString()       // Retorna o tipo de café que o Cliente quer
+    {
+        return Coffie;
+    }
+}
