c3c96b6f4afc7e94ce83d57b9b8953984430c474
Pepper Ikeoka <pedroikeoka@gmail.com>
2025-03-19 17:05:27 -0300

    feat: processamento do cafe

--- Assets/Scripts/FinalProductProcessing.cs
+++ Assets/Scripts/FinalProductProcessing.cs
@@ -1,9 +1,20 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 
-public class FinalProductProcessing : MonoBehaviour {
+public class FinalProductProcessing : MonoBehaviour
+{
 
     public string productName; // Nome do produto
+
+    // Dicionario que determina o nome do produto final com base nos ingredientes e processos utilizados
+    public Dictionary<string, string> coffeeDict = new Dictionary<string, string> {
+        {"1,1,1,0,0,0", "caféA"},
+        {"1,2,1,0,0,0", "caféB"}
+    };
+
+    public bool occupied = false;
 
     // Ingredientes colocados no produto, determinam o nome do produto
     int ingredientA;
@@ -16,14 +27,15 @@ public class FinalProductProcessing : MonoBehaviour {
     public bool processC = false;
 
     // Metodo chamado quando um ingrediente é adicionado
-    public void OnDropIngredient(IngredientCarrier drop) {
+    public void OnDropIngredient(IngredientCarrier drop)
+    {
 
         IngredientType dropIngredient = drop.type;
 
-        switch (dropIngredient) {                           // Switch pra determinar qual ingrediente deve ser adicionado
+        switch (dropIngredient)
+        {                           // Switch pra determinar qual ingrediente deve ser adicionado
             case IngredientType.ingredientA:
                 ingredientA++;
-                
                 Debug.Log($"IngredientA: {ingredientA} " +
                     $"\nIngredientB: {ingredientB} " +
                     $"\nIngredientC: {ingredientC}");
@@ -42,25 +54,39 @@ public class FinalProductProcessing : MonoBehaviour {
             case IngredientType.ingredientC:
                 ingredientC++;
 
-                Debug.Log($"IngredientA: {ingredientA} " + 
-                    $"\nIngredientB: {ingredientB} " + 
+                Debug.Log($"IngredientA: {ingredientA} " +
+                    $"\nIngredientB: {ingredientB} " +
                     $"\nIngredientC: {ingredientC}");
 
                 break;
         }
 
-        //UpdateProduct();
+        UpdateProduct();
 
     }
 
-    
+
 
     // Metodo pra determinar o nome do produto com base nos ingredientes adicionados
     //
     // NOTA: Talvez seja prudente o uso de um botão que "finaliza" o café para que o
     // código não rode toda vez que um ingrediente é acrescentado
-    private void UpdateProduct() {
-        
+    private void UpdateProduct()
+    {
+        int[] _ingreds = { ingredientA, ingredientB, ingredientC,                  // Junta os ingredientes
+                           processA ? 1 : 0, processB ? 1 : 0, processC ? 1 : 0 }; //       e processos em 1 array (bools viram 1 ou 0)
+        string _key = string.Join(",", _ingreds);                                  // Transforma o array em string pra ser usado com o dicionario
+        if (coffeeDict.TryGetValue(_key, out string _name))                        // Ve se a combinação existe no dicionario
+        {
+            productName = _name;                                                   // Se existir, atualiza o nome do produto
+        }
+        else
+        {
+            productName = "Invalid";                                               // Se nao, atualiza pra indicar que a combinação nao é valida
+        }
+
+        Debug.Log(_key);
+        Debug.Log(productName);
     }
 
 }
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Interaction : MonoBehaviour {
+public class Interaction : MonoBehaviour
+{
 
     // ============================================
     // || Esse script fica no MANAGER da cena    ||
@@ -24,10 +25,11 @@ public class Interaction : MonoBehaviour {
     // Posição na tela para que objetos movidos acompanhem o dedo do jogador
     public Vector3 cursorPos;
 
-    
+
 
     // Definindo variaveis
-    private void Awake() {
+    private void Awake()
+    {
         mainCamera = Camera.main;
         playerInput = GetComponent<PlayerInput>();
 
@@ -42,51 +44,68 @@ public class Interaction : MonoBehaviour {
     }
 
     // Ativando e desativando mapas de ações do InputActions
-    private void OnEnable() {
-        touchAction.Enable(); 
+    private void OnEnable()
+    {
+        touchAction.Enable();
         touchAction.performed += touchPressed; // Inscreve o metodo ao evento touchAction.performed (quando o jogador toca na tela)
     }
-    private void OnDisable() {
+    private void OnDisable()
+    {
         touchAction.performed -= touchPressed; // Desinscreve quando o script é desativado
         touchAction.Disable();
     }
 
     // Metodo pro cursor (cursorPos) acompanhar onde o jogador encosta ou ta encostando na tela
-    private void UpdateCursorPosition() {
+    private void UpdateCursorPosition()
+    {
         cursorPos = mainCamera.ScreenToWorldPoint(positionAction.ReadValue<Vector2>());
         cursorPos.z = Z_ABSOLUTE_SCENE_POS;
     }
 
-    
+
     // Metodo chamado quando o jogo recebe o input de toque na tela
-    private void touchPressed(InputAction.CallbackContext context) {
+    private void touchPressed(InputAction.CallbackContext context)
+    {
         UpdateCursorPosition();
         RaycastHit2D hit = Physics2D.Raycast(cursorPos, Vector2.zero); // Raycast pra detectar objetos
-        if (hit.collider != null) {
+        if (hit.collider != null)
+        {
             SelectParser(hit);
         }
     }
 
     // Metodo pra determinar que objeto foi detectado primeiro pelo raycast
-    private void SelectParser(RaycastHit2D hit) {
-        if (hit.collider.GetComponent<Ingredient>()) {               // Para Ingredientes
+    private void SelectParser(RaycastHit2D hit)
+    {
+        if (hit.collider.GetComponent<Ingredient>())
+        {               // Para Ingredientes
             hit.collider.GetComponent<Ingredient>().OnClicked();
-        } else if (hit.collider.CompareTag("Product")) {             // Para o produto final (Café)
-            StartCoroutine(DragProduct(hit.transform.gameObject));
+        }
+        else if (hit.collider.CompareTag("Product"))
+        {             // Para o produto final (Café)
+            if (hit.transform.GetComponent<FinalProductProcessing>().occupied == false)
+            {
+                StartCoroutine(DragProduct(hit.transform.gameObject));
+            }
         }
     }
 
     // Corrotina pra arrastar ingredientes pela tela
-    public IEnumerator DragIngredient(GameObject obj) {
-        while (touchAction.ReadValue<float>() != 0) {
+    public IEnumerator DragIngredient(GameObject obj)
+    {
+        while (touchAction.ReadValue<float>() != 0)
+        {
             UpdateCursorPosition();
             obj.transform.position = cursorPos;
             yield return waitForFixedUpdate;
         }
-        if (touchAction.ReadValue<float>() == 0) {
+        if (touchAction.ReadValue<float>() == 0)
+        {
             RaycastHit2D[] hits = Physics2D.RaycastAll(cursorPos, Vector2.zero);
-            foreach (RaycastHit2D hit in hits) {
-                if (hit.transform.CompareTag("Product")) {
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.transform.CompareTag("Product"))
+                {
                     hit.transform.GetComponent<FinalProductProcessing>().OnDropIngredient(obj.GetComponent<IngredientCarrier>());
                 }
             }
@@ -95,12 +114,25 @@ public class Interaction : MonoBehaviour {
     }
 
     // Corrotina pra arrastar o produto até alguma maquina que altere o produto
-    // INCOMPLETO
-    public IEnumerator DragProduct(GameObject obj) {
-        while (touchAction.ReadValue < float>() != 0) {
+    public IEnumerator DragProduct(GameObject obj)
+    {
+        while (touchAction.ReadValue<float>() != 0)
+        {
             UpdateCursorPosition();
             obj.transform.position = cursorPos;
             yield return waitForFixedUpdate;
+        }
+        if (touchAction.ReadValue<float>() == 0)
+        {
+            RaycastHit2D[] hits = Physics2D.RaycastAll(cursorPos, Vector2.zero);
+            foreach (RaycastHit2D hit in hits)
+            {
+                if (hit.transform.CompareTag("Process"))
+                {
+                    hit.transform.GetComponent<CoffeeProcess>().OnDropProduct(obj.GetComponent<FinalProductProcessing>());
+                    obj.transform.position = hit.transform.position;
+                }
+            }
         }
     }
 
