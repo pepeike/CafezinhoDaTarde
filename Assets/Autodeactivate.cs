679fc86ec25e52de95265828a3df1fe3cd749bca
UnusualSether <lucaspelzel@gmail.com>
2025-05-27 09:41:36 -0300

    Trying to fix Loading Transition in cafe scene
    *Trying to fix a problem where cut out transition from loading scene to main scene is broken, however i think this is a Unity bug as eve when i delay the animation it still doesn't play.

--- Assets/Autodeactivate.cs
+++ Assets/Autodeactivate.cs
@@ -2,8 +2,23 @@ using UnityEngine;
 
 public class Autodeactivate : MonoBehaviour
 {
-   private void Deactivate()
+    public Animator Anim;
+
+
+    private void Start()
     {
+        Invoke("ShowAnimation", 10f);
+    }
+
+
+    private void ShowAnimation()
+    {
+        Anim.SetTrigger("OnStartScene");
+        
+    }
+    private void Deactivate()
+    {
+        
         this.gameObject.SetActive(false);
     }
 }
