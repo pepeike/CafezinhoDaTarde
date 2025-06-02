068b4e9068c85add7690f0ef571c25a74c220da9
Pepper Ikeoka <pedroikeoka@gmail.com>
2025-06-02 16:29:52 -0300

    feat: Brewing Process (still bugged)

---
+++ Assets/Scripts/CoffeeBrewer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class CoffeeBrewer : MonoBehaviour
+{
+    public enum BrewerState {
+        Idle,
+        Brewing,
+        Finished
+    }
+
+    public BrewerState currentState = BrewerState.Idle;
+    public Ingredient processedIngredient;
+    public IngredientCarrier carrier;
+    public Ingredient liquidIngredient;
+    //public IngredientCarrier liquidCarrier;
+    public FinalProductProcessing cup;
+
+    public void OnDropIngred(Ingredient ingred, IngredientCarrier carry) {
+        if (!ingred.isLiquid) {
+            liquidIngredient = ingred; // Armazena o ingrediente l�quido (��gua ou leite)
+            carrier = carry; // Recebe o carrier do ingrediente
+            carrier.transform.position += new Vector3(-200, -200, 0);
+            //carrier.ToggleVisible(); // Desativa a visibilidade do ingrediente enquanto está sendo processado
+            StartCoroutine(BrewingProcess());
+        } else if (ingred.isLiquid) {
+            processedIngredient = ingred; // Armazena o ingrediente seco (gr��o de caf�� ou ch��)
+            StartCoroutine(MilkProcess());
+        }
+
+        
+
+        
+        
+        
+
+    }
+
+    public void OnDropCup(FinalProductProcessing fpp) {
+        cup = fpp; // Recebe a instância do copo onde o produto final será colocado
+        if (currentState == BrewerState.Finished) {
+            OnBrewingFinished(); // Se o preparo já estiver concluído, chama o método de finalização
+        } else {
+            Debug.Log("Aguardando preparo da bebida...");
+        }
+    }
+
+    IEnumerator BrewingProcess() {
+        currentState = BrewerState.Brewing;
+        Debug.Log("Iniciando preparo da bebida...");
+        // Simula o tempo de preparo
+        yield return new WaitForSeconds(3f);
+        OnBrewingFinished();
+    }
+
+    IEnumerator MilkProcess() {
+        currentState = BrewerState.Brewing;
+        yield return new WaitForSeconds(3f);
+        OnMilkFinished();
+    }
+
+    private void OnMilkFinished() {
+        if (cup != null) {
+            int outEffect = liquidIngredient.GetEffect();
+            cup.OnPourLiquid(outEffect);
+        }
+    }
+
+    public void OnBrewingFinished() {
+        currentState = BrewerState.Finished;
+        Debug.Log("Preparo concluído!");
+        // Aqui você pode adicionar a lógica para o que acontece após o preparo
+        // Por exemplo, você pode ativar um efeito visual ou sonoro
+        // Adiciona o ingrediente processado ao copo
+        if (cup != null) {
+            
+            //int outEffect = processedIngredient.GetEffect();
+            //int outTaste = processedIngredient.isBean ? 0 : 1; // Efeito do ingrediente processado
+
+            cup.OnPourDrink(processedIngredient.isBean ? 0 : 1, processedIngredient.GetEffect());
+            carrier.ToggleVisible();
+            Invoke("Flush", 2f); // Reseta o estado do brewer e limpa os ingredientes
+        }
+    }
+
+    
+
+    public void Flush() {
+        // Reseta o estado do brewer e limpa os ingredientes
+        currentState = BrewerState.Idle;
+        processedIngredient = null;
+        liquidIngredient = null;
+        carrier = null;
+        cup = null;
+        Debug.Log("Brewer resetado.");
+    }
+
+}
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,8 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Interaction : MonoBehaviour
-{
+public class Interaction : MonoBehaviour {
 
     // ============================================
     // || Esse script fica no MANAGER da cena    ||
@@ -28,8 +27,7 @@ public class Interaction : MonoBehaviour
 
 
     // Definindo variaveis
-    private void Awake()
-    {
+    private void Awake() {
         mainCamera = Camera.main;
         playerInput = GetComponent<PlayerInput>();
 
@@ -44,94 +42,108 @@ public class Interaction : MonoBehaviour
     }
 
     // Ativando e desativando mapas de ações do InputActions
-    private void OnEnable()
-    {
+    private void OnEnable() {
         touchAction.Enable();
         touchAction.performed += touchPressed; // Inscreve o metodo ao evento touchAction.performed (quando o jogador toca na tela)
     }
-    private void OnDisable()
-    {
+    private void OnDisable() {
         touchAction.performed -= touchPressed; // Desinscreve quando o script é desativado
         touchAction.Disable();
     }
 
     // Metodo pro cursor (cursorPos) acompanhar onde o jogador encosta ou ta encostando na tela
-    private void UpdateCursorPosition()
-    {
+    private void UpdateCursorPosition() {
         cursorPos = mainCamera.ScreenToWorldPoint(positionAction.ReadValue<Vector2>());
         cursorPos.z = Z_ABSOLUTE_SCENE_POS;
     }
 
 
     // Metodo chamado quando o jogo recebe o input de toque na tela
-    private void touchPressed(InputAction.CallbackContext context)
-    {
+    private void touchPressed(InputAction.CallbackContext context) {
         UpdateCursorPosition();
         RaycastHit2D hit = Physics2D.Raycast(cursorPos, Vector2.zero); // Raycast pra detectar objetos
-        if (hit.collider != null)
-        {
+        if (hit.collider != null) {
             SelectParser(hit);
         }
     }
 
     // Metodo pra determinar que objeto foi detectado primeiro pelo raycast
-    private void SelectParser(RaycastHit2D hit)
-    {
-        if (hit.collider.GetComponent<Ingredient>())
-        {               // Para Ingredientes
-            hit.collider.GetComponent<Ingredient>().OnClicked();
-        }
-        else if (hit.collider.CompareTag("Product"))
-        {             // Para o produto final (Café)
-            if (hit.transform.GetComponent<FinalProductProcessing>().occupied == false)
-            {
+    private void SelectParser(RaycastHit2D hit) {
+        if (hit.collider.GetComponent<IngredientSpawner>()) {               // Para Ingredientes
+            hit.collider.GetComponent<IngredientSpawner>().OnClicked();
+            return;
+        } else if (hit.collider.CompareTag("Milk")) {
+            StartCoroutine(DragIngredient(hit.transform.gameObject)); // Para ingredientes líquidos (água, leite, etc.)
+        } else if (hit.collider.CompareTag("Product")) {             // Para o produto final (Café)
+            if (hit.transform.GetComponent<FinalProductProcessing>().occupied == false) {
                 StartCoroutine(DragProduct(hit.transform.gameObject));
+            }
+        } else if (hit.collider.CompareTag("Grinder")) {
+            if (hit.transform.GetComponent<CoffeeGrinder>().currentState == CoffeeGrinder.GrinderState.Finished) {
+                StartCoroutine(DragIngredient(hit.transform.GetComponent<CoffeeGrinder>().SpawnGroundIngred()));
+            }
+        } else if (hit.collider.CompareTag("Ingredient")) {
+            if (hit.transform.GetComponent<IngredientCarrier>().isVisible == true) {
+                StartCoroutine(DragIngredient(hit.transform.gameObject)); // Para ingredientes que podem ser arrastados
             }
         }
     }
 
     // Corrotina pra arrastar ingredientes pela tela
-    public IEnumerator DragIngredient(GameObject obj)
-    {
-        while (touchAction.ReadValue<float>() != 0)
-        {
+    public IEnumerator DragIngredient(GameObject obj) {
+        while (touchAction.ReadValue<float>() != 0) {
             UpdateCursorPosition();
             obj.transform.position = cursorPos;
             yield return waitForFixedUpdate;
         }
-        if (touchAction.ReadValue<float>() == 0)
-        {
+        if (touchAction.ReadValue<float>() == 0) {
             RaycastHit2D[] hits = Physics2D.RaycastAll(cursorPos, Vector2.zero);
-            foreach (RaycastHit2D hit in hits)
-            {
-                if (hit.transform.CompareTag("Product"))
-                {
-                    hit.transform.GetComponent<FinalProductProcessing>().OnDropIngredient(obj.GetComponent<IngredientCarrier>());
+            DragToParser(hits, obj); // Chama o metodo que verifica onde o ingrediente foi largado
+            //Destroy(obj);
+        }
+    }
+
+    void DragToParser(RaycastHit2D[] hits, GameObject obj) {
+        foreach (RaycastHit2D hit in hits) {
+            //if (hit.transform.CompareTag("Product")) {
+            //    hit.transform.GetComponent<FinalProductProcessing>().OnDropIngredient(obj.GetComponent<Ingredient>());
+            //} else
+            Debug.Log(hit.transform.name + " - " + hit.transform.tag);
+
+            if (hit.transform.CompareTag("Grinder")) {
+                if (hit.transform.GetComponent<CoffeeGrinder>().currentState == CoffeeGrinder.GrinderState.Idle) {
+                    hit.transform.GetComponent<CoffeeGrinder>().StartGrinding(obj.GetComponent<IngredientCarrier>().ingred, obj.GetComponent<IngredientCarrier>());
+                }
+            } else if (hit.transform.CompareTag("Brewer")) {
+                hit.transform.GetComponent<CoffeeBrewer>().OnDropIngred(obj.GetComponent<IngredientCarrier>().ingred, obj.GetComponent<IngredientCarrier>());
+            }
+
+            if (hits.Length == 0) {
+                if (obj.GetComponent<IngredientCarrier>().ingred.isLiquid == false) {
+                    Destroy(obj); // Se o ingrediente não for liquido, destrói o objeto
                 }
             }
-            Destroy(obj);
         }
     }
 
     // Corrotina pra arrastar o produto até alguma maquina que altere o produto
-    public IEnumerator DragProduct(GameObject obj)
-    {
-        while (touchAction.ReadValue<float>() != 0)
-        {
+    public IEnumerator DragProduct(GameObject obj) {
+        while (touchAction.ReadValue<float>() != 0) {
             UpdateCursorPosition();
             obj.transform.position = cursorPos;
             yield return waitForFixedUpdate;
         }
-        if (touchAction.ReadValue<float>() == 0)
-        {
+        if (touchAction.ReadValue<float>() == 0) {
             RaycastHit2D[] hits = Physics2D.RaycastAll(cursorPos, Vector2.zero);
-            foreach (RaycastHit2D hit in hits)
-            {
-                if (hit.transform.CompareTag("Process"))
-                {
-                    hit.transform.GetComponent<CoffeeProcess>().OnDropProduct(obj.GetComponent<FinalProductProcessing>());
-                    obj.transform.position = hit.transform.position;
-                }
+            DragProductToParser(obj, hits);
+        }
+    }
+
+    private void DragProductToParser(GameObject obj, RaycastHit2D[] hits) {
+        foreach (RaycastHit2D hit in hits) {
+            if (hit.transform.CompareTag("Brewer")) {
+                hit.transform.GetComponent<CoffeeBrewer>().OnDropCup(obj.GetComponent<FinalProductProcessing>());
+                obj.transform.position = hit.transform.position;
             }
         }
     }
